179c0d41688df1aff2c4b62d2de1f21bc5739d08
Desmond Beramendi <desmond.beramendi@octoconsulting.com>
2020-01-15 09:56:29 -0500

    tried another path in .sh file

--- task_show_stuff.sh
+++ task_show_stuff.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #!/bin/bash
 
-git update-index --chmod=+x ./task_show_stuff.sh
+git update-index --chmod=+x ./run-on-new-commit/task_show_stuff.sh
 
 get_hostname=$(hostname)
 echo "Hostname is ${get_hostname}"
